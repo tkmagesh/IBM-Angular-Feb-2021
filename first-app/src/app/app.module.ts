84024e41422c096d005441f52285510d1436f23f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-02 11:48:01 +0530

    using ngFor - assignment

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   /* register the UI entities of the module. ie., Component, Directive & Pipe */
   declarations: [
     AppComponent,
     GreeterComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ProductsComponent
   ],
   /* register the other modules that this module depends on */
   imports: [
