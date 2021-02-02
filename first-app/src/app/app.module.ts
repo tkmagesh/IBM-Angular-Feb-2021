b7ba404d23957fa2ca6ebae0facc4c9dfe0c250e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-02 09:53:43 +0530

    greeter implementation - part - 1

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GreeterComponent } from './greeter/greeter.component';
 
 @NgModule({
   /* register the UI entities of the module. ie., Component, Directive & Pipe */
   declarations: [
-    AppComponent
+    AppComponent,
+    GreeterComponent
   ],
   /* register the other modules that this module depends on */
   imports: [
     BrowserModule
+    , FormsModule
   ],
   /* register the non-UI entites. ie., Service */
   providers: [],
---
+++ first-app/src/app/greeter/greeter.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greeter',
+  templateUrl: './greeter.component.html',
+  styleUrls: ['./greeter.component.css']
+})
+export class GreeterComponent implements OnInit {
+
+  userName : string = '';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
