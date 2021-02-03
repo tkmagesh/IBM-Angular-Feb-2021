76276129f86e2417ea5789fc45b128335776967e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-03 10:44:43 +0530

    implementing TrimTextPipe

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bug-tracker/bug-tracker.component';
 
+import { TrimTextPipe } from './bug-tracker/pipes/trimText.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
-    BugTrackerComponent
+    BugTrackerComponent,
+    TrimTextPipe
   ],
   imports: [
     BrowserModule,
