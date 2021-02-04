288b63c480fbdfaffe944ce9786077494ca2cdc7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-04 11:44:28 +0530

    using changeDetection (with OnPush)

--- bug-tracker-app/src/app/bug-tracker/components/bug-stats/bug-stats.component.ts
+++ bug-tracker-app/src/app/bug-tracker/components/bug-stats/bug-stats.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { Bug } from "../../models/Bug";
 
 @Component({
     selector : 'app-bug-stats',
     template : `
+        <div>{{ getCurrentTime() }}</div>
         <section class="stats">
             <span class="closed">{{ bugs | closedCount }}</span>
             <span> / </span>
             <span>{{bugs.length}}</span>
         </section>
     `,
-    styleUrls : ['bug-stats.component.css']
+    styleUrls : ['bug-stats.component.css'],
+    changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class BugStatsComponent{
 
     @Input('list')
     bugs : Bug[] = [];
+
+    getCurrentTime(){
+        return new Date();
+    }
 }
