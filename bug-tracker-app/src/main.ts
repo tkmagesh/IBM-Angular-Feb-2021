de8abeaff98a8fac7e74d6a8a3813ab55d3ad131
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-04 12:05:53 +0530

    using moment

--- bug-tracker-app/src/main.ts
+++ bug-tracker-app/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//npm install moment
+import * as moment from 'moment';
+console.log(moment('2021-01-07T08:03:20.771Z').fromNow());
+
 if (environment.production) {
   enableProdMode();
 }
