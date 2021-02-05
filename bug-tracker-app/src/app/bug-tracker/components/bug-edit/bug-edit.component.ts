891cf5722376767b171b21929321ff376926403c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-02-05 14:58:44 +0530

    form validation for bug edit component

--- bug-tracker-app/src/app/bug-tracker/components/bug-edit/bug-edit.component.ts
+++ bug-tracker-app/src/app/bug-tracker/components/bug-edit/bug-edit.component.ts
@@ -5,11 +5,39 @@ import { BugOperationsService } from "../../services/bugOperations.service";
 @Component({
   selector : 'app-bug-edit',
   template : `
-    <section class="edit">
-        <label for="">Bug Name :</label>
-        <input type="text" [(ngModel)]="newBugName">
-        <input type="button" value="Add New" (click)="onAddNewClick()">
-    </section>
+    <form #bugForm="ngForm">
+        <span>Form is Invalid : {{bugForm.invalid}}</span>
+        
+        <section class="edit">
+            <label for="">Bug Name :</label>
+            <input 
+                required 
+                name="bugName" 
+                #bugName="ngModel" 
+                type="text" 
+                [(ngModel)]="newBugName"
+                [ngClass]="{errorInput : bugName.touched && bugName.invalid}"    
+            >
+            <div class="errorMessage" *ngIf="bugName.touched && bugName.invalid">
+                Bug Name is mandatory!
+            </div>
+            <hr/>
+            <div>
+                <div>Touched : {{bugName.touched}} </div>
+                <div>Untouched : {{bugName.untouched}} </div>
+                <div>Pristine : {{bugName.pristine}} </div>
+                <div>Dirty : {{bugName.dirty}} </div>
+                <div>valid : {{bugName.valid}} </div>
+                <div>Invalid : {{bugName.invalid}} </div>
+            </div>
+            <hr/>
+            <input 
+                type="button" 
+                value="Add New" (click)="onAddNewClick()"
+                [disabled]="bugForm.invalid"
+            >
+        </section>
+    </form>
   `,
   styleUrls : ['./bug-edit.component.css']
 })
